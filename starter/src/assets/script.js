8ad53a4712d7a2f24925e2f4a7a38c375052ee9a
Vanessa Michelle <vanessamichelle580@gmail.com>
2024-11-18 11:26:22 -0600

    Added product images

--- starter/src/assets/script.js
+++ starter/src/assets/script.js
@@ -1,5 +1,5 @@
 /* Create an array named products which you will use to add all of your product object literals that you create in the next step. */
-
+const products = [
 /* Create 3 or more product objects using object literal notation 
    Each product should include five properties
    - name: name of product (string)
@@ -8,14 +8,67 @@
    - productId: unique id for the product (number)
    - image: picture of product (url string)
 */
+{
+  name: 'Orange',
+  price: 3,
+  quantity: 0,
+  productId: 1,
+  image: 'starter\src\images\orange.jpg'
+},
+
+{
+  name: 'Apple',
+  price: 2.50,
+  quantity: 0,
+  productId: 2,
+  image: 'starter\src\images\apple.jpg'
+},
+
+{
+  name: 'Banana',
+  price: .85,
+  quantity: 0,
+  productId: 3,
+  image: 'starter\src\images\banana.jpg'
+},
+
+{
+  name: 'Cherry',
+  price: 4,
+  quantity: 0,
+  productId: 4,
+  image: 'starter\src\images\cherry.jpg' 
+},
+
+{
+  name: 'Strawberry',
+  price: 4.50,
+  quantity: 0,
+  productId: 5,
+  image: 'starter\src\images\strawberry.jpg'
+},
+
+{
+  name: 'Raspberry',
+  price: 3.25,
+  quantity: 0,
+  productId: 6,
+  image: 'starter\src\images\raspberry.jpg'
+}
+];
 
 /* Images provided in /images folder. All images from Unsplash.com
    - cherry.jpg by Mae Mu
    - orange.jpg by Mae Mu
    - strawberry.jpg by Allec Gomes
+   - apple.jpg by Elena Koycheva
+   - banana.jpg by Tim Foster
+   - raspberry.jpg by Hamad Alahamad
 */
 
 /* Declare an empty array named cart to hold the items in the cart */
+
+const cart = [];
 
 /* Create a function named addProductToCart that takes in the product productId as an argument
   - addProductToCart should get the correct product based on the productId
